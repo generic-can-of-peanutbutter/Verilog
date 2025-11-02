1b726d9ec6128660a1989f9e7c91ded0621cc0e3
Priyansh <priyanshsarkar04@gmail.com>
2025-11-03 01:23:31 +0530

    Add files via upload

---
+++ 4 bit Full Adder/FA.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.04.2025 00:33:35
+// Design Name: 
+// Module Name: FA
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FA(
+
+    );
+endmodule
