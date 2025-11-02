0bceabf5ecaf3cdad4ac368098f9fe8156581168
Priyansh <priyanshsarkar04@gmail.com>
2025-11-03 02:00:51 +0530

    Add files via upload

---
+++ Binary to Decimal/b2d.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.04.2025 21:14:22
+// Design Name: 
+// Module Name: b2d
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module b2d(
+
+    );
+endmodule
