0d90f726dec730c71c41f123bc67c7859386e072
Priyansh <priyanshsarkar04@gmail.com>
2025-11-03 01:30:40 +0530

    Add files via upload

---
+++ Active low 2 to 4 decoder/decoder.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.04.2025 00:58:26
+// Design Name: 
+// Module Name: decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module decoder(
+    input [1:0]a,
+    output [3:0] y
+    );
+endmodule
