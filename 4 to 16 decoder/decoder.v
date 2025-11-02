b665bb9394afd3d9cc106edbe136469adae64aa9
Priyansh <priyanshsarkar04@gmail.com>
2025-11-03 01:25:48 +0530

    Add files via upload

---
+++ 4 to 16 decoder/decoder.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.04.2025 17:45:31
+// Design Name: 
+// Module Name: decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module decoder(
+    input [3:0]I,
+    output [15:0]Y
+    );
+endmodule
